a635f515879be13f3799ba2fc364910c7c0fd90d
kiran <kiraninbox18995@gmail.com>
2024-07-12 14:37:26 +0530

    refactored

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.kiran.movie"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kiran.movie"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
