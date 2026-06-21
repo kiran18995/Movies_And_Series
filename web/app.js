dcad2b0e0b48c5e3be532e07a10fd03770cdb9b5
Kiran Kumar N <kiraninbox18995@gmail.com>
2026-06-22 00:42:29 +0530

    Switch streaming provider from streamimdb to vidsrc.me

--- web/app.js
+++ web/app.js
@@ -452,8 +452,8 @@ function openPlayer(item) {
 
 function launchPlayer(title, isTV, item, season, episode) {
   const url = item.isMovie 
-    ? `https://streamimdb.ru/embed/movie/${item.id}` 
-    : `https://streamimdb.ru/embed/tv/${item.id}/${season}/${episode}`;
+    ? `https://vidsrc.me/embed/movie?tmdb=${item.id}` 
+    : `https://vidsrc.me/embed/tv?tmdb=${item.id}&season=${season}&episode=${episode}`;
 
   document.getElementById('playerTitle').textContent = title;
   
